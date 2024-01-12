fc278193b71fca79f6ec388775ceb66e1fc06a2b
Harinandhana1 <154225224+Harinandhana1@users.noreply.github.com>
2024-01-12 14:52:20 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public fixedSIPAmount;
+    uint256 public flexibleSIPAmount;
+    uint256 public flexibleSIPIncrementRate;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event FixedSIP(uint256 amount);
+    event FlexibleSIP(uint256 amount, uint256 incrementRate);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        fixedSIPAmount = 0;
+        flexibleSIPAmount = 0;
+        flexibleSIPIncrementRate = 10;
     }
 
     function getBalance() public view returns(uint256){
@@ -22,20 +28,15 @@ contract Assessment {
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -48,15 +49,35 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
     }
+
+    function startFixedSIP(uint256 _sipAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        fixedSIPAmount = _sipAmount;
+
+        emit FixedSIP(_sipAmount);
+    }
+
+    function startFlexibleSIP(uint256 _sipAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        flexibleSIPAmount = _sipAmount;
+
+        emit FlexibleSIP(_sipAmount, flexibleSIPIncrementRate);
+    }
+
+    function incrementFlexibleSIP() public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(flexibleSIPAmount > 0, "Flexible SIP not started");
+
+        uint256 incrementAmount = (flexibleSIPAmount * flexibleSIPIncrementRate) / 100;
+
+        flexibleSIPAmount += incrementAmount;
+
+        emit FlexibleSIP(flexibleSIPAmount, flexibleSIPIncrementRate);
+    }
 }
-
-
